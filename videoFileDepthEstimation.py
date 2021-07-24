445b4135a0c2025c22d915569494fbab953c78ed
dpredie <you@example.com>
2021-07-23 21:02:55 -0400

    adding video file for colab
    adding video file for colab

---
+++ videoFileDepthEstimation.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+from MidasDepthEstimation.midasDepthEstimator import midasDepthEstimator
+import time
+import argparse
+
+inputfile = "/content/inputs/input.mp4"
+outputfolder= "/content/results"
+fps=24.0
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--output_file', type=str, required=True, help='copy path from output directory')
+parser.add_argument('--input_file', type=str, required=True, help='copy path from input file')
+parser.add_argument('--fps', type=int, required=True, help='fps')
+
+
+# Check for required input
+option_, _ = parser.parse_known_args()
+print(option_)
+outputfile = option_.output_file
+inputfile = option_.input_file
+fps = option_.fps
+
+# Initialize depth estimation model
+depthEstimator = midasDepthEstimator()
+
+# Initialize video
+cap = cv2.VideoCapture(inputfile)
+
+print(cap)
+out = cv2.VideoWriter(outputfile,cv2.VideoWriter_fourcc(*'MP4V'), fps,(int(cap.get(3)*2),int(cap.get(4))))
+
+if (cap.isOpened()== False):
+  print("Error opening video stream or file")
+
+framecnt = 0
+while cap.isOpened():
+
+  # Read frame from the video
+  ret, img = cap.read()
+
+  if ret:  
+    start = time.time()
+    #now = datetime.datetime.now()
+    # Estimate depth
+    colorDepth = depthEstimator.estimateDepth(img)
+
+    # Add the depth image over the color image:
+    combinedImg = cv2.addWeighted(img,0.7,colorDepth,0.6,0)
+
+    # Join the input image, the estiamted depth and the combined image
+    img_out = np.hstack((img, colorDepth))
+    #print(img_out)
+    #cv2.imshow("Depth Image", img_out)
+    out.write(img_out)
+    end = time.time()
+    print("processed frame: "+str(framecnt)+" "+str("%.2f" % (end-start))+"s")
+    framecnt+=1
+  else:
+    print("image empty - exiting")    
+    break  
+  # Press key q to stop
+  if cv2.waitKey(1) == ord('q'):
+    break
+  
+
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
